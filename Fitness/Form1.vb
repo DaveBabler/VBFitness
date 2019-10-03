c67e1c9ac0eb844fa80735dfeb24cedf83bb51d0
Dave Babler <dbabler@dealercontrols.com>
2019-10-03 14:30:17 -0400

    Begin code comments

--- Fitness/Form1.vb
+++ Fitness/Form1.vb
@@ -1,6 +1,15 @@
-﻿Public Class frmFitness
+﻿' Program Name: FitnessChallenge
+' Author:       Dave Babler
+' Date:         2019-10-03
+' Purpose:      Program collects then displays a team's individual weight loss numbers
+'               then the program calculates and displays the average (MEAN) weight loss for the entire team.
+Option Strict On
+Public Class frmFitness
     Private Sub frmFitness_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
+    Private Sub btnWeightLoss_Click(sender As Object, e As EventArgs) Handles btnWeightLoss.Click
+        'Button calculates up to 8 weight loss values and then gets the mathematical mean for the team.
+    End Sub
 End Class
